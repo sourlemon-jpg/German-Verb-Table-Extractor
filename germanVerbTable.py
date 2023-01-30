a7e68c779fdb7f5965816d5e1392f89af6323bb7
sourlemon-jpg <71064969+sourlemon-jpg@users.noreply.github.com>
2023-01-30 17:10:25 +0300

    Update germanVerbTable.py

--- germanVerbTable.py
+++ germanVerbTable.py
@@ -7,7 +7,6 @@ from colorama import Fore, Back, Style
 istek = input("Enter a verb: ")
 url = f"https://en.pons.com/verb-tables/german/{istek}"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-url2 = f"https://context.reverso.net/translation/german-english/{istek}"
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 # Find all the tables with class "ft-single-table"
